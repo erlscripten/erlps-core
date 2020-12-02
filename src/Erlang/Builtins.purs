cf3189ec3120c2d1d8d3af196163bec40fe45ed9
Grzegorz Uriasz <gorbak25@gmail.com>
2020-12-02 17:21:59 +0100

    Fix -- operator2

--- src/Erlang/Builtins.purs
+++ src/Erlang/Builtins.purs
@@ -505,9 +505,9 @@ erlang__op_unAppend args = EXC.badarity (ErlangFun 2 purs_tco_sucks {-erlang__op
 
 do_unappend ErlangEmptyList ErlangEmptyList ErlangEmptyList = ErlangEmptyList
 do_unappend r@(ErlangCons _ _) ErlangEmptyList ErlangEmptyList = r
-do_unappend ErlangEmptyList ErlangEmptyList acc = lists__reverse__2 [acc]
+do_unappend ErlangEmptyList ErlangEmptyList acc = lists__reverse__2 [acc, ErlangEmptyList]
 do_unappend (ErlangCons h t) ErlangEmptyList acc = do_unappend t ErlangEmptyList (ErlangCons h acc)
-do_unappend ErlangEmptyList (ErlangCons _ term) acc = do_unappend (lists__reverse__2 [acc]) term ErlangEmptyList
+do_unappend ErlangEmptyList (ErlangCons _ term) acc = do_unappend (lists__reverse__2 [acc, ErlangEmptyList]) term ErlangEmptyList
 do_unappend (ErlangCons hl tl) r@(ErlangCons hr tr) acc =
       case erlang__op_exactEq [hl, hr] of
         ErlangAtom "true"  -> do_unappend tl tr acc

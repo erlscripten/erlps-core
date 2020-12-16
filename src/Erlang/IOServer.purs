684c1950ca6cf5e839d76a7502cf1fc64cd2b05a
Grzegorz Uriasz <gorbak25@gmail.com>
2020-12-16 22:49:01 +0100

    better ioserver

--- src/Erlang/IOServer.purs
+++ src/Erlang/IOServer.purs
@@ -11,6 +11,7 @@ import Erlang.Type
 import Data.String.CodePoints as StrCP
 import Data.String as Str
 import Unsafe.Coerce
+import Erlang.Builtins as BIF
 
 wololo_codepoint :: Partial => ErlangTerm -> StrCP.CodePoint
 wololo_codepoint (ErlangInt res) = unsafeCoerce res
@@ -24,6 +25,12 @@ erlps__request__2 [io_server@(ErlangPID _), ErlangTuple [ErlangAtom "io_request"
         ErlangTuple [ErlangAtom "io_reply", replyAs, handle_request request]
 erlps__request__2 args = ErlangAtom "ok"
 
+handle_request (ErlangTuple [ErlangAtom "put_chars", ErlangAtom "unicode", iolist]) =
+    let
+        str = BIF.erlang__binary_to_list__1 [BIF.erlang__iolist_to_binary__1 [iolist]]
+        a = unsafePerformEffect (log ((toString str)))
+    in
+          ErlangAtom "ok"
 handle_request (ErlangTuple [ErlangAtom "put_chars", ErlangAtom "unicode", ErlangAtom "io_lib", ErlangAtom "format", ErlangCons format (ErlangCons ErlangEmptyList ErlangEmptyList)]) =
     let
         a = unsafePerformEffect (log ((toString format)))
